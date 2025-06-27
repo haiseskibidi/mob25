395a48656c287462f55ed2ddce026d5c32e9af3a
SkloniKavkazVPokloneNizkom <akopalexx@mail.ru>
2025-06-27 13:22:19 +1000

    Hello world

---
+++ app/src/main/java/ru/fefu/heeello/HelloWorld.kt
@@ -0,0 +1,12 @@
+package ru.fefu.heeello
+
+import android.app.Activity
+import android.os.Bundle
+
+class HelloWorld : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_hello_world)
+    }
+}
